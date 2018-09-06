32eb9cc194f363fbe1142baf36719433d348e0c8
SAnjiHolic <sanhah0322@naver.com>
2018-09-07 01:11:16 +0900

    Auth run

--- run.sh
+++ run.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/bash
+if [ ! -f ./search ]; then
+	make
+fi
 
-make;
-./search | perl -n smali_injection.pl;
+if [ "$#" -lt 1 ]; then
+	echo "Entrt directory to search.";
+else
+	if [ ! -d $1 ];then
+		echo "The argument must be directory."
+	else
+		if [ -d $1_change ]; then
+			rm -rf $1_change
+		fi
+		cp -r $1 $1_change
+		./search $1_change | perl -n smali_injection.pl;
+	fi
+fi
